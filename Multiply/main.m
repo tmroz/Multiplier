6678bb6fa5dd30876d048085bd3f73262d391d29
tmroz <tom.mroz@d214.org>
2014-05-12 15:12:35 -0500

    Initial Commit

---
+++ Multiply/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Multiply
+//
+//  Created by Thomas M. Mroz on 5/12/14.
+//  Copyright (c) 2014 BGHS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ MultiplyTests/MultiplyTests.m
@@ -0,0 +1,34 @@
+//
+//  MultiplyTests.m
+//  MultiplyTests
+//
+//  Created by Thomas M. Mroz on 5/12/14.
+//  Copyright (c) 2014 BGHS. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MultiplyTests : XCTestCase
+
+@end
+
+@implementation MultiplyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
