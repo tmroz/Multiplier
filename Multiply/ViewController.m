a338c4de432dd4ca192b9ea5b9b6860b4aee92ca
tmroz <tom.mroz@d214.org>
2014-05-13 08:09:17 -0500

    finished

--- Multiply/ViewController.m
+++ Multiply/ViewController.m
@@ -9,6 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *myNumber;
+
+@property (weak, nonatomic) IBOutlet UILabel *myMultiplier;
+@property (weak, nonatomic) IBOutlet UILabel *myAnswer;
+@property (weak, nonatomic) IBOutlet UISlider *mySlider;
+
 
 @end
 
@@ -19,6 +25,88 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
+- (void)setMultiplier
+{
+    NSInteger newMult = self.mySlider.value;
+    self.myMultiplier.text = [NSString stringWithFormat: @"%d", newMult];
+}
+
+-(void) updateFromSlider
+{
+
+        NSInteger number = [self.myNumber.text integerValue];
+        NSInteger multiple = [self.myMultiplier.text integerValue];
+        NSInteger result = number * multiple;
+
+
+        if(result > 20)
+        {
+            self.view.backgroundColor = [UIColor yellowColor];
+        }
+
+        if ((result %3 == 0) && (result %5 == 0))
+        {
+            self.myAnswer.text = @"fizzbuzz";
+
+        }
+        else if (result %3 == 0)
+        {
+            self.myAnswer.text = @"fizz";
+        }
+        else if (result %5 == 0)
+        {
+            self.myAnswer.text = @"buzz";
+        }
+        else
+            self.myAnswer.text = [NSString stringWithFormat:@"%d", result];
+
+
+}
+
+- (IBAction)onSliderMoved:(id)sender
+{
+    [self setMultiplier];
+    [self updateFromSlider];
+
+    if(self.mySlider.value < 10.0)
+    {
+        self.mySlider.tintColor = [UIColor redColor];
+    }
+    else
+    {
+        self.mySlider.tintColor = [UIColor blueColor];
+    }
+}
+
+- (IBAction)onCalculateButtonPressed:(id)sender
+{
+    NSInteger number = [self.myNumber.text integerValue];
+    NSInteger multiple = [self.myMultiplier.text integerValue];
+    NSInteger result = number * multiple;
+
+
+    if(result > 20)
+    {
+        self.view.backgroundColor = [UIColor greenColor];
+    }
+
+    if ((result %3 == 0) && (result %5 == 0))
+    {
+        self.myAnswer.text = @"fizzbuzz";
+
+    }
+    else if (result %3 == 0)
+    {
+        self.myAnswer.text = @"fizz";
+    }
+    else if (result %5 == 0)
+    {
+        self.myAnswer.text = @"buzz";
+    }
+    else
+        self.myAnswer.text = [NSString stringWithFormat:@"%d", result];
+}
+
 
 - (void)didReceiveMemoryWarning
 {
